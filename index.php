c8509ce4007d0087e8b3ec8e90761b415a1bc967
Alex Sirotin <semieway@gmail.com>
2021-03-07 14:02:49 +0500

    Create index file with api routing

--- index.php
+++ index.php
@@ -1,1 +1,17 @@
 <?php
+
+use App\Api;
+use App\Database;
+
+require_once 'vendor/autoload.php';
+
+// Route to API.
+if (preg_match('/^\/api\/.*/', $_SERVER['REQUEST_URI'])) {
+    $db = new Database();
+    $requestData = json_decode(file_get_contents('php://input'), TRUE) ?? [];
+    $api = new Api($_SERVER['REQUEST_URI'], $_SERVER['REQUEST_METHOD'], $db, $requestData);
+    $response = $api->getJsonResponse();
+
+    header('Content-Type: application/json; charset=utf-8');
+    echo $response;
+}
